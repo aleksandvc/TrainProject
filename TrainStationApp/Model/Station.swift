b47866fd2d849d5d395030f562524d9f865889c0
alexander.tsvetanov <alexander.tsvetanov@upnetix.com>
2020-07-02 16:20:39 +0300

    Adding first functionallity and some UI

---
+++ TrainStationApp/Model/Station.swift
@@ -0,0 +1,18 @@
+//
+//  Station.swift
+//  TrainStationApp
+//
+//  Created by alexander.tsvetanov on 1.07.20.
+//  Copyright © 2020 alexander.tsvetanov. All rights reserved.
+//
+
+import Foundation
+
+struct Station {
+    let description: String
+    let alias: String
+    let latitude: Double
+    let longtitude: Double
+    let code: String
+    let id: Int
+}
